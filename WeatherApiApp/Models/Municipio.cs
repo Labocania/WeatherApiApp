620a7e17ddbd9b44cbf66ffc196874b1b9021704
Labocania <hugokl@outlook.com>
2021-07-13 20:32:48 -0300

    Adicionado modelo de requisições da API OpenUV, ReqOpenUV.

--- WeatherApiApp/Models/Municipio.cs
+++ WeatherApiApp/Models/Municipio.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WeatherApiApp.Models
@@ -14,5 +15,7 @@ namespace WeatherApiApp.Models
 
         [Timestamp]
         public byte[] UltimaModificacao { get; set; }
+
+        public ICollection<ReqOpenUV> RequisicoesOpenUV { get; set; }
     }
 }
