8ba3fc97221153bcdf454b89d526b853df1b730b
Labocania <hugokl@outlook.com>
2021-07-13 19:39:34 -0300

    Começando a criar arquivo para popular tabela de Municípios.

---
+++ WeatherApiApp/Models/MunicipioSeedData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WeatherApiApp.Data;
+
+namespace WeatherApiApp.Models
+{
+    public static class MunicipioSeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+            {
+                if (context.Municipios.Any())
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
